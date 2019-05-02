f19f2223aa2931b4b3944471ecf86cdfbd10d4ef
mtraynham <matt.traynham@pureport.com>
2019-05-02 17:32:16 -0400

    Document IKE options

--- ansible/modules/pureport/pureport_site_ipsec_vpn_connection.py
+++ ansible/modules/pureport/pureport_site_ipsec_vpn_connection.py
@@ -47,6 +47,109 @@ options:
         type: str
         choices: ['V1', 'V2']
         default: 'V2'
+    ike_v1_ike_encryption:
+        description:
+            - The IKE V1 IKE Encryption algorithm
+        required: true
+        type: str
+        choices: ['AES_128', 'AES_192', 'AES_256']
+        default: 'AES_128'
+    ike_v1_ike_integrity:
+        description:
+            - The IKE V1 IKE Integrity algorithm
+        required: true
+        type: str
+        choices: ['MD5_HMAC', 'SHA1_HMAC', 'SHA256_HMAC', 'SHA384_HMAC', 'SHA512_HMAC']
+        default: 'SHA256_HMAC'
+    ike_v1_ike_dh_group:
+        description:
+            - The IKE V1 Diffie-Hellman group
+        required: true
+        type: str
+        choices: ['MODP_1024', 'MODP_1536', 'MODP_2048', 'MODP_3072', 'MODP_4096', 'MODP_6144',
+                  'MODP_8192', 'ECP_192', 'ECP_224', 'ECP_256', 'ECP_384', 'ECP_521']
+        default: 'MODP_2048'
+    ike_v1_esp_encryption:
+        description:
+            - The IKE V1 ESP Encryption algorithm
+        required: true
+        type: str
+        choices: ['NULL', 'AES_128', 'AES_192', 'AES_256', 'AES_128_CTR', 'AES_192_CTR', 'AES_256_CTR',
+                  'AES_128_GCM_64', 'AES_192_GCM_64', 'AES_256_GCM_64', 'AES_128_GCM_96', 'AES_192_GCM_96',
+                  'AES_256_GCM_96', 'AES_128_GCM_128', 'AES_192_GCM_128', 'AES_256_GCM_128', 'AES_128_GMAC',
+                  'AES_192_GMAC', 'AES_256_GMAC']
+        default: 'AES_128'
+    ike_v1_esp_integrity:
+        description:
+            - The IKE V1 ESP Integrity algorithm
+        required: true
+        type: str
+        choices: ['MD5_HMAC', 'SHA1_HMAC', 'SHA256_HMAC', 'SHA384_HMAC', 'SHA512_HMAC', 'AES_XCBC']
+        default: 'SHA256_HMAC'
+    ike_v1_esp_dh_group:
+        description:
+            - The IKE V1 Diffie-Hellman algorithm
+        required: true
+        type: str
+        choices: ['MODP_1024', 'MODP_1536', 'MODP_2048', 'MODP_3072', 'MODP_4096', 'MODP_6144',
+                  'MODP_8192', 'ECP_192', 'ECP_224', 'ECP_256', 'ECP_384', 'ECP_521']
+        default: 'MODP_2048'
+    ike_v2_ike_encryption:
+        description:
+            - The IKE V2 IKE Encryption algorithm
+        required: true
+        type: str
+        choices: ['NULL', 'AES_128', 'AES_192', 'AES_256', 'AES_128_CTR', 'AES_192_CTR', 'AES_256_CTR',
+                  'AES_128_GCM_64', 'AES_192_GCM_64', 'AES_256_GCM_64', 'AES_128_GCM_96', 'AES_192_GCM_96',
+                  'AES_256_GCM_96', 'AES_128_GCM_128', 'AES_192_GCM_128', 'AES_256_GCM_128']
+        default: 'AES_128'
+    ike_v2_ike_integrity:
+        description:
+            - The IKE V2 IKE Integrity algorithm
+        required: true
+        type: str
+        choices: ['MD5_HMAC', 'SHA1_HMAC', 'SHA256_HMAC', 'SHA384_HMAC', 'SHA512_HMAC', 'AES_XCBC']
+        default: 'SHA256_HMAC'
+    ike_v2_ike_prf:
+        description:
+            - The IKE V2 IKE Pseudo-random Function
+        required: true
+        type: str
+        choices: ['MD5', 'SHA_1', 'AES_XCBC', 'SHA_256', 'SHA_384', 'SHA_512']
+        default: 'SHA_256'
+    ike_v2_ike_dh_group:
+        description:
+            - The IKE V2 Diffie-Hellman group
+        required: true
+        type: str
+        choices: ['MODP_1024', 'MODP_1536', 'MODP_2048', 'MODP_3072', 'MODP_4096', 'MODP_6144', 'MODP_8192',
+                  'ECP_192', 'ECP_224', 'ECP_256', 'ECP_384', 'ECP_521']
+        default: 'MODP_2048'
+    ike_v2_esp_encryption:
+        description:
+            - The IKE V2 ESP Encryption algorithm
+        required: true
+        type: str
+        choices: ['NULL', 'AES_128', 'AES_192', 'AES_256', 'AES_128_CTR', 'AES_192_CTR', 'AES_256_CTR',
+                  'AES_128_GCM_64', 'AES_192_GCM_64', 'AES_256_GCM_64', 'AES_128_GCM_96', 'AES_192_GCM_96',
+                  'AES_256_GCM_96', 'AES_128_GCM_128', 'AES_192_GCM_128', 'AES_256_GCM_128', 'AES_128_GMAC',
+                  'AES_192_GMAC', 'AES_256_GMAC']
+        default: 'AES_128'
+    ike_v2_esp_integrity:
+        description:
+            - The IKE V2 ESP Integrity algorithm
+        required: true
+        type: str
+        choices: ['MD5_HMAC', 'SHA1_HMAC', 'SHA256_HMAC', 'SHA384_HMAC', 'SHA512_HMAC', 'AES_XCBC']
+        default: 'SHA256_HMAC'
+    ike_v2_esp_dh_group:
+        description:
+            - The IKE V2 Diffie-Hellman algorithm
+        required: true
+        type: str
+        choices: ['MODP_1024', 'MODP_1536', 'MODP_2048', 'MODP_3072', 'MODP_4096', 'MODP_6144', 'MODP_8192',
+                  'ECP_192', 'ECP_224', 'ECP_256', 'ECP_384', 'ECP_521']
+        default: 'MODP_2048'
     primary_key:
         description:
             - The IPSec pre-shared key for the secondary gateway.
@@ -57,16 +160,6 @@ options:
             - The IPSec pre-shared key for the secondary gateway.
         required: false
         type: str
-    ike_v1:
-        description:
-            - The IKEV1 configuration.
-        required: false
-        type: dict
-    ike_v2:
-        description:
-            - The IKEV2 configuration.
-        required: false
-        type: dict
     traffic_selectors:
         description:
             - A list of traffic selectors (e.g dict(customer_side=str, pureport_side=str))
@@ -113,6 +206,22 @@ from ansible.module_utils.pureport.pureport_connection_crud import \
     connection_crud
 
 
+__IKE_AEAD_ALGORITHMS = [
+    'AES_128_GCM_64',
+    'AES_192_GCM_64',
+    'AES_256_GCM_64',
+    'AES_128_GCM_96',
+    'AES_192_GCM_96',
+    'AES_256_GCM_96',
+    'AES_128_GCM_128',
+    'AES_192_GCM_128',
+    'AES_256_GCM_128',
+    'AES_128_GMAC',
+    'AES_192_GMAC',
+    'AES_256_GMAC'
+]
+
+
 def construct_connection(module):
     """
     Construct a Connection from the Ansible module arguments
@@ -137,11 +246,50 @@ def construct_connection(module):
         'ike_version',
         'primary_key',
         'secondary_key',
-        'ike_v1',
-        'ike_v2',
         'traffic_selectors',
         'enable_bgp_password'
     ))
+    if connection.get('ike_version') == 'V1':
+        connection.update(dict(
+            ikeV1=dict(
+                ike=dict((k[11:], module.params.get(k)) for k in (
+                    'ike_v1_ike_encryption',
+                    'ike_v1_ike_integrity',
+                    'ike_v1_ike_dh_group'
+                )),
+                esp=dict((k[11:], module.params.get(k)) for k in (
+                    'ike_v1_esp_encryption',
+                    'ike_v1_esp_integrity',
+                    'ike_v1_esp_dh_group'
+                ))
+            )
+        ))
+        if connection['ikeV1']['esp']['encryption'] in __IKE_AEAD_ALGORITHMS:
+            connection['ikeV1']['esp'].pop('integrity')
+    else:
+        connection.update(dict(
+            ikeV2=dict(
+                ike=dict((k[11:], module.params.get(k)) for k in (
+                    'ike_v2_ike_encryption',
+                    'ike_v2_ike_integrity',
+                    'ike_v2_ike_prf',
+                    'ike_v2_ike_dh_group'
+                )),
+                esp=dict((k[11:], module.params.get(k)) for k in (
+                    'ike_v2_esp_encryption',
+                    'ike_v2_esp_integrity',
+                    'ike_v2_esp_dh_group'
+                ))
+            )
+        ))
+        if connection['ikeV2']['ike']['encryption'] in __IKE_AEAD_ALGORITHMS:
+            connection['ikeV2']['ike'].pop('integrity')
+        else:
+            connection['ikeV2']['ike'].pop('prf')
+
+        if connection['ikeV2']['ike']['encryption'] in __IKE_AEAD_ALGORITHMS:
+            connection['ikeV2']['ike'].pop('integrity')
+
     connection.update(dict(
         type="SITE_IPSEC_VPN",
         authType="PSK"
@@ -179,19 +327,244 @@ def main():
             ),
             physical_address=dict(type="dict"),
             ike_version=dict(type='str', choices=['V1', 'V2'], default='V2'),
+            ike_v1_ike_encryption=dict(
+                type='str',
+                choices=[
+                    'AES_128',
+                    'AES_192',
+                    'AES_256'
+                ],
+                default='AES_128'
+            ),
+            ike_v1_ike_integrity=dict(
+                type='str',
+                choices=[
+                    'MD5_HMAC',
+                    'SHA1_HMAC',
+                    'SHA256_HMAC',
+                    'SHA384_HMAC',
+                    'SHA512_HMAC'
+                ],
+                default='SHA256_HMAC'
+            ),
+            ike_v1_ike_dh_group=dict(
+                type='str',
+                choices=[
+                    'MODP_1024',
+                    'MODP_1536',
+                    'MODP_2048',
+                    'MODP_3072',
+                    'MODP_4096',
+                    'MODP_6144',
+                    'MODP_8192',
+                    'ECP_192',
+                    'ECP_224',
+                    'ECP_256',
+                    'ECP_384',
+                    'ECP_521'
+                ],
+                default='MODP_2048'
+            ),
+            ike_v1_esp_encryption=dict(
+                type='str',
+                choices=[
+                    'NULL',
+                    'AES_128',
+                    'AES_192',
+                    'AES_256',
+                    'AES_128_CTR',
+                    'AES_192_CTR',
+                    'AES_256_CTR',
+                    'AES_128_GCM_64',
+                    'AES_192_GCM_64',
+                    'AES_256_GCM_64',
+                    'AES_128_GCM_96',
+                    'AES_192_GCM_96',
+                    'AES_256_GCM_96',
+                    'AES_128_GCM_128',
+                    'AES_192_GCM_128',
+                    'AES_256_GCM_128',
+                    'AES_128_GMAC',
+                    'AES_192_GMAC',
+                    'AES_256_GMAC'
+                ],
+                default='AES_128'
+            ),
+            ike_v1_esp_integrity=dict(
+                type='str',
+                choices=[
+                    'MD5_HMAC',
+                    'SHA1_HMAC',
+                    'SHA256_HMAC',
+                    'SHA384_HMAC',
+                    'SHA512_HMAC',
+                    'AES_XCBC'
+                ],
+                default='SHA256_HMAC'
+            ),
+            ike_v1_esp_dh_group=dict(
+                type='str',
+                choices=[
+                    'MODP_1024',
+                    'MODP_1536',
+                    'MODP_2048',
+                    'MODP_3072',
+                    'MODP_4096',
+                    'MODP_6144',
+                    'MODP_8192',
+                    'ECP_192',
+                    'ECP_224',
+                    'ECP_256',
+                    'ECP_384',
+                    'ECP_521'
+                ],
+                default='MODP_2048'
+            ),
+            ike_v2_ike_encryption=dict(
+                type='str',
+                choices=[
+                    'NULL',
+                    'AES_128',
+                    'AES_192',
+                    'AES_256',
+                    'AES_128_CTR',
+                    'AES_192_CTR',
+                    'AES_256_CTR',
+                    'AES_128_GCM_64',
+                    'AES_192_GCM_64',
+                    'AES_256_GCM_64',
+                    'AES_128_GCM_96',
+                    'AES_192_GCM_96',
+                    'AES_256_GCM_96',
+                    'AES_128_GCM_128',
+                    'AES_192_GCM_128',
+                    'AES_256_GCM_128'
+                ],
+                default='AES_128'
+            ),
+            ike_v2_ike_integrity=dict(
+                type='str',
+                choices=[
+                    'MD5_HMAC',
+                    'SHA1_HMAC',
+                    'SHA256_HMAC',
+                    'SHA384_HMAC',
+                    'SHA512_HMAC',
+                    'AES_XCBC'
+                ],
+                default='SHA256_HMAC'
+            ),
+            ike_v2_ike_prf=dict(
+                type='str',
+                choices=[
+                    'MD5',
+                    'SHA_1',
+                    'AES_XCBC',
+                    'SHA_256',
+                    'SHA_384',
+                    'SHA_512'
+                ],
+                default='SHA_256'
+            ),
+            ike_v2_ike_dh_group=dict(
+                type='str',
+                choices=[
+                    'MODP_1024',
+                    'MODP_1536',
+                    'MODP_2048',
+                    'MODP_3072',
+                    'MODP_4096',
+                    'MODP_6144',
+                    'MODP_8192',
+                    'ECP_192',
+                    'ECP_224',
+                    'ECP_256',
+                    'ECP_384',
+                    'ECP_521'
+                ],
+                default='MODP_2048'
+            ),
+            ike_v2_esp_encryption=dict(
+                type='str',
+                choices=[
+                    'NULL',
+                    'AES_128',
+                    'AES_192',
+                    'AES_256',
+                    'AES_128_CTR',
+                    'AES_192_CTR',
+                    'AES_256_CTR',
+                    'AES_128_GCM_64',
+                    'AES_192_GCM_64',
+                    'AES_256_GCM_64',
+                    'AES_128_GCM_96',
+                    'AES_192_GCM_96',
+                    'AES_256_GCM_96',
+                    'AES_128_GCM_128',
+                    'AES_192_GCM_128',
+                    'AES_256_GCM_128',
+                    'AES_128_GMAC',
+                    'AES_192_GMAC',
+                    'AES_256_GMAC'
+                ],
+                default='AES_128'
+            ),
+            ike_v2_esp_integrity=dict(
+                type='str',
+                choices=[
+                    'MD5_HMAC',
+                    'SHA1_HMAC',
+                    'SHA256_HMAC',
+                    'SHA384_HMAC',
+                    'SHA512_HMAC',
+                    'AES_XCBC'
+                ],
+                default='SHA256_HMAC'
+            ),
+            ike_v2_esp_dh_group=dict(
+                type='str',
+                choices=[
+                    'MODP_1024',
+                    'MODP_1536',
+                    'MODP_2048',
+                    'MODP_3072',
+                    'MODP_4096',
+                    'MODP_6144',
+                    'MODP_8192',
+                    'ECP_192',
+                    'ECP_224',
+                    'ECP_256',
+                    'ECP_384',
+                    'ECP_521'
+                ],
+                default='MODP_2048'
+            ),
             primary_key=dict(type='str'),
             secondary_key=dict(type='str'),
-            ike_v1=dict(type='dict'),
-            ike_v2=dict(type='dict'),
             traffic_selectors=dict(type='list', default=[]),
             enable_bgp_password=dict(type='bool')
         )
     )
     mutually_exclusive = []
     mutually_exclusive += get_network_mutually_exclusive()
-    mutually_exclusive += [
-        ['ike_v1', 'ike_v2']
-    ]
+    mutually_exclusive += \
+        [[ike_v1_field, ike_v2_field]
+         for ike_v1_field in [
+             'ike_v1_ike_encryption',
+             'ike_v1_ike_integrity',
+             'ike_v1_ike_dh_group',
+             'ike_v1_esp_encryption',
+             'ike_v1_esp_integrity',
+             'ike_v1_esp_dh_group']
+         for ike_v2_field in [
+             'ike_v2_ike_encryption',
+             'ike_v2_ike_integrity',
+             'ike_v2_ike_prf',
+             'ike_v2_ike_dh_group',
+             'ike_v2_esp_encryption',
+             'ike_v2_esp_integrity',
+             'ike_v2_esp_dh_group']
+         ]
     module = AnsibleModule(
         argument_spec=argument_spec,
         mutually_exclusive=mutually_exclusive
