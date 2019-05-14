cd660d0f09a7560711509d7ef1d5738bdc4672d6
mtraynham <matt.traynham@pureport.com>
2019-05-14 09:27:38 -0400

    Bump develop version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pureport-ansible-modules',
-    version='0.0.1',
+    version='0.0.2',
     author='Pureport',
     author_email='noreply@pureport.com',
     license='GPLv3',
